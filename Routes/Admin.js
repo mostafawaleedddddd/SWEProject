4f186b0e42278bd2f907b1988a80bb1a686efd0a
Omar400-cpu <omardarhous123456@gmail.com>
2024-10-29 23:57:58 +0300

    Update

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -1,10 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// // const User=require('../controllers/User')
-// router.get('/', (req, res) => {
-//     res.render('Admin',{Admin: (req.session.Admin === undefined ? "" : req.session.Admin)});
-// });
-
 // module.exports = router;
 const express = require('express');
 const router = express.Router();
