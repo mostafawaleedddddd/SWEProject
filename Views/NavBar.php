365d61777057e4e211e451f137c552b28f42f2b5
Ryderclaw <ramezemadef@gmail.com>
2024-12-08 19:13:49 +0200

    php

--- Views/NavBar.php
+++ Views/NavBar.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_type'])) {
+    $_SESSION['user_type'] = 'guest'; 
+}
+$user_type = $_SESSION['user_type']; 
+?>
 <html>
 
 <head>
@@ -6,14 +13,27 @@
 
 <body>
 	<nav class="navbar">
-		<div class="logo"><a href="/"><img src="/Medira/Media/images/LOGO.png"></a> </div>
+		<div class="logo"><a href="/Medira/Views/index.php"><img src="/Medira/Media/images/LOGO.png"></a> </div>
 		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/DiscussionForum">Discussion Forum</a></li>
-			<li><a href="/ContactUs">Contact Us</a></li>
-			<button class="Signup" onclick="location.href='/Medira/Views/signup.php';">SignUp</button>
-			<button class="SignIn" onclick="location.href='/Medira/Views/login.php';">SignIn</button>
-		</ul>
+            <?php if ($user_type == 'healthcare'): ?>
+                <li><a href="/Medira/Views/index.php">Home</a></li>
+                <li><a href="/Medira/Views/DiscussionForum.php">Discussion Forum </a></li>
+            <?php elseif ($user_type == 'admin'): ?>
+                <li><a href="/Medira/Views/Admin.php">Admin Home</a></li>
+                <li><a href="/Manage">Manage Users</a></li>
+                <li><a href="/Medira/Views/Analytics.php">Analytics</a></li>
+            <?php elseif ($user_type == 'patient'): ?>
+                <li><a href="/">Home</a></li>
+                <li><a href="/Medira/Views/DiscussionForum.php">Discussion Forum </a></li>
+                <li><a href="/Medira/Views/ContuctUs.php">Contuct Us</a></li>
+            <?php else: ?>
+				<li><a href="/Medira/Views/index.php">Home</a></li>
+				<li><a href="/Medira/Views/DiscussionForum.php">Discussion Forum </a></li>
+                <li><a href="/Medira/Views/ContuctUs.php">Contuct Us</a></li>
+				<button class="Signup" onclick="location.href='/Medira/Views/signup.php';">SignUp</button>
+				<button class="SignIn" onclick="location.href='/Medira/Views/login.php';">SignIn</button>
+            <?php endif; ?>
+        </ul>
 	</nav>
 </body>
 
--- Views/index.php
+++ Views/index.php
@@ -1,5 +1,13 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_type'])) {
+  $_SESSION['user_type'] = 'guest';
+}
+$user_type = $_SESSION['user_type'];
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,33 +15,46 @@
   <link rel="stylesheet" href="/Medira/Media/css/NavBar.css">
   <link rel="stylesheet" href="/Medira/Media/css/Styles.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
   <title>Medira</title>
 </head>
+
 <body>
   <div id="navbar">
-  <?php include "NavBar.php"; ?>
+    <?php include "NavBar.php"; ?>
   </div>
   <div class="image-container">
-     
+
     <p><b>Welcome to Medira</b></p>
-    <h2>Empowering healthcare professionals and patients with accurate medical <br>answers. Discover how our NLP-driven system enhances clinical decision-<br>making.</h2>
+    <h2>Empowering healthcare professionals and patients with accurate medical <br>answers. Discover how our NLP-driven
+      system enhances clinical decision-<br>making.</h2>
     <div class="button-container">
-        <button id="start"class="Started" onclick="location.href='/Signup';">Get Started</button>
+      <?php if ($user_type == 'healthcare'): ?>
+
+      <?php elseif ($user_type == 'admin'): ?>
+
+      <?php elseif ($user_type == 'patient'): ?>
+        <button id="start" class="Started" onclick="location.href='/Medira/Views/Chat.php';">Start Chatting</button>
+      <?php else: ?>
+        <button id="start" class="Started" onclick="location.href='/Medira/Views/Signup.php';">Get Started</button>
+      <?php endif; ?>
     </div>
   </div>
   <footer>
     <div class="footer-content">
-      <h3><img src="/Medira/Media/images/LOGO.png" alt="Medira Logo" class="footer-logo"></h3> 
+      <h3><img src="/Medira/Media/images/LOGO.png" alt="Medira Logo" class="footer-logo"></h3>
       <nav>
         <ul>
           <li><a href="/DiscussionForum">Discussion Forum</a></li>
         </ul>
       </nav>
-      <p>&copy; 2024 Medira. All rights reserved. </p>Empowering healthcare professionals and patients with reliable medical information.
+      <p>&copy; 2024 Medira. All rights reserved. </p>Empowering healthcare professionals and patients with reliable
+      medical information.
     </div>
   </footer>
-  <div class="feedback-button" id="feedbackButton" onclick="location.href='/Feedback';">Feedback</div>  
+  <div class="feedback-button" id="feedbackButton" onclick="location.href='/Feedback';">Feedback</div>
 </body>
+
 </html>
