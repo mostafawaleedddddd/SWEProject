eef0e0bef1d535060861927ea5c6fd5050fed355
Ryderclaw <ramezemadef@gmail.com>
2024-12-20 21:13:23 +0200

    ContactUs

--- Views/ContactUs.php
+++ Views/ContactUs.php
@@ -1,55 +1,65 @@
+<?php
+require_once '../Models/ContactUs.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+ $name=$_POST["name"];
+ $username=$_POST["username"];
+ $message=$_POST["message"];
+ $model=new Contact($name,$message,$username);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link
-    href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
-    rel="stylesheet"
-/>
-<link rel="stylesheet" href="/Medira/Media/css/Styles.css">
-<link rel="stylesheet" href="/Medira/Media/css/Feedback.css">
-<link rel="stylesheet" href="/Medira/Media/css/ContactUs.css">
-<link rel="stylesheet" href="/Medira/Media/css/NavBar.css">
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
+  <link rel="stylesheet" href="/Medira/Media/css/Styles.css">
+  <link rel="stylesheet" href="/Medira/Media/css/Feedback.css">
+  <link rel="stylesheet" href="/Medira/Media/css/ContactUs.css">
+  <link rel="stylesheet" href="/Medira/Media/css/NavBar.css">
 </head>
+
 <body>
   <div id="navbar">
     <?php include "NavBar.php"; ?>
   </div>
   <div class="container">
-<div class="left-content">
-<h3>Get in touch</h3>
-<p>We are here for you! Now can we help?</p>
-<form action="">
-<label for="name">Name</label>
-<input type="text" name="" id="name">
+    <div class="left-content">
+      <h3>Get in touch</h3>
+      <p>We are here for you! Now can we help?</p>
+      <form action="ContactUs.php" method="post">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
 
-<label for="email">Email</label>
-<input type="email" name="" id="email">
+        <label for="email">Email</label>
+        <input type="email" name="username" id="email">
 
-<label for="message">Message</label>
-<textarea name="" id="textarea" rows="5" cols="50"></textarea>
-<input class="btn" type="submit" value="Submit">
-</form>
-</div>
-<div class="right-content">
-  <img src="/Medira/Media/images/Contact us-amico.png">
-  <div class="contact-info">
-    <div>
-      <i class="ri-map-pin-line"></i>
-      <span> 54 share3 Mabna Almain gnb far8al </span>
+        <label for="message">Message</label>
+        <textarea name="message" id="textarea" rows="5" cols="50"></textarea>
+        <input class="btn" type="submit" value="Submit">
+      </form>
     </div>
-    <div>
-      <i class="ri-phone-fill"></i>
-      <span> 45465462561</span>
+    <div class="right-content">
+      <img src="/Medira/Media/images/Contact us-amico.png">
+      <div class="contact-info">
+        <div>
+          <i class="ri-map-pin-line"></i>
+          <span> 54 share3 Mabna Almain gnb far8al </span>
+        </div>
+        <div>
+          <i class="ri-phone-fill"></i>
+          <span> 45465462561</span>
+        </div>
+        <div>
+          <i class="ri-mail-line"></i>
+          <span> medira@outlook.com</span>
+        </div>
+      </div>
     </div>
-    <div>
-      <i class="ri-mail-line"></i>
-      <span> medira@outlook.com</span>
-    </div>
-  </div>
-</div>
   </div>
   <div class="social-btns">
     <a href="">
@@ -62,6 +72,7 @@
       <i class="ri-linkedin-fill"></i>
     </a>
   </div>
-  <div class="feedback-button" id="feedbackButton" onclick="location.href='/Medira/Views/Feedback.php';">Feedback</div>  
+  <div class="feedback-button" id="feedbackButton" onclick="location.href='/Medira/Views/Feedback.php';">Feedback</div>
 </body>
+
 </html>
